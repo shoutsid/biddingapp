a17545ae6853d3ba0e8e044bf194763d142f9e91
James <shoutsid@gmail.com>
2013-05-14 20:09:55 +0100

    used it for validation

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,10 +1,6 @@
 class Bid < ActiveRecord::Base
   validates_numericality_of :price, on: :create,
-    greater_than: Proc.new { |bid|
-    bid.item.highest_bid ?
-      bid.item.highest_bid.price.to_f :
-      bid.item.starting_price 
-  }
+    greater_than: Proc.new { |bid| highest_bid(bid) }
   after_create :update_item
   belongs_to :user
   belongs_to :item
@@ -17,10 +13,9 @@ class Bid < ActiveRecord::Base
   end
 
   def self.highest_bid(bid)
-    @bid = bid
-    @item = @bid.item
-    @highest_bid = @item.highest_bid
+    item = bid.item
+    highest_bid = item.highest_bid
 
-    @highest_bid ? @highest_bid.price.to_f : @item.starting_price
+    highest_bid ? highest_bid.price.to_f : item.starting_price
   end
 end
