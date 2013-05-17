9b452c5d041a2ac7e42cc73638113e850ce586fa
James <shoutsid@gmail.com>
2013-05-17 17:27:26 +0100

    Tests to match changes

--- spec/model/item_spec.rb
+++ spec/model/item_spec.rb
@@ -8,4 +8,63 @@ describe Item do
   it { should validate_presence_of(:starting_price) }
   it { should validate_presence_of(:closing_time) }
   it { should have_many(:bids) }
+  it { should have_one(:highest_bid) }
+
+  describe '#time_left' do
+    context 'there is time left' do
+      it 'should give correct time left in seconds' do
+        item = FactoryGirl.create(:item)
+        item.time_left.should eql("Time left: " + item.check_time_left.to_s + " Seconds")
+      end
+      
+      it 'returns type of string' do
+        item = FactoryGirl.create(:item)
+        item.time_left.should be_kind_of(String)
+      end
+    end
+
+    context 'theres no time left' do
+      it 'should return correct string' do
+        item = FactoryGirl.create(:item, closing_time: Time.now - 2.hours)
+        item.time_left.should eql("This Item Has Expired")
+      end
+    end
+  end
+
+  describe '#expired?' do
+    context 'there is time left' do
+      it 'returns false' do
+        item = FactoryGirl.create(:item)
+        item.expired?.should eql(false)
+      end
+    end
+
+    context 'theres no time left' do
+      it 'returns true' do
+        item = FactoryGirl.create(:item, closing_time: Time.now - 2.hours)
+        item.expired?.should eql(true)
+      end
+    end
+  end
+
+  describe '#increase_time_left' do
+    it 'increased time left by 10 seconds' do
+        item = FactoryGirl.create(:item)
+        before_change = item.check_time_left
+        item.increase_time_left
+        Item.find(item).check_time_left.should eql(before_change + 10.seconds)
+    end
+  end
+
+  describe 'check_time_left' do
+    it 'returns as an integer' do
+      item = FactoryGirl.create(:item)
+      item.check_time_left.should be_kind_of(Integer)
+    end
+
+    it 'returns correct time left in seconds' do
+      item = FactoryGirl.create(:item, closing_time: Time.now + 1.hour)
+      item.check_time_left.should eql(1.hour.to_i)
+    end
+  end
 end
