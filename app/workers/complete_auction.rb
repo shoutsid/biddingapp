5174a4656075a84db59dfda415d4ec30f1c3a391
James <shoutsid@gmail.com>
2013-05-20 01:45:31 +0100

    bg process loop to try and complete auction

---
+++ app/workers/complete_auction.rb
@@ -0,0 +1,20 @@
+class CompleteAuction
+  @queue = :check_complete_auction_queue
+
+  def self.perform(item_id)
+    begin 
+      loop do
+        item = Item.find(item_id)
+        if item.expired?
+          puts "name: #{item.name}, item_id: #{item.id}, bid_id: #{item.highest_bid.id if item.highest_bid}, bid_amount: #{item.highest_bid.amount if item.highest_bid}"
+          item.close_auction
+
+          ## TODO:- Add some payment method here
+          #         Create some live feed data of all auctions
+        end
+        break if item.closed?
+        sleep 2
+      end
+    end
+  end
+end
