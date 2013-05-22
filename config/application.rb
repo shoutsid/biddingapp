d572ad1ef644d682d2ab7c2c2483d806fe65ba2e
James <shoutsid@gmail.com>
2013-05-22 23:15:24 +0100

    preload all lib files

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Bidding
   class Application < Rails::Application
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     # Settings in config/environments/* take precedence over those specified here.
+    config.autoload_paths += %W(#{config.root}/lib)
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
