75fa11c97e98050a5926234b1943ae67f4b55696
James <shoutsid@gmail.com>
2013-05-23 14:37:41 +0100

    Override strong params for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :address, :street_number, :locality, :administrative_area_level_1, :administrative_area_level_2, :country, :postal_code) }
+  end
+
 end
