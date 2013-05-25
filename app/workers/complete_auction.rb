e512000faeecdf2795954b2d66e23704a0ac10ff
James <shoutsid@gmail.com>
2013-05-25 08:56:00 +0100

    make sure to break, will constantly loop otherwise

--- app/workers/complete_auction.rb
+++ app/workers/complete_auction.rb
@@ -10,6 +10,7 @@ class CompleteAuction
         if item.expired?
           puts "name: #{item.name}, item_id: #{item.id}, bid_id: #{item.highest_bid.id if item.highest_bid}, bid_amount: #{item.highest_bid.amount if item.highest_bid}"
           item.close_auction
+          break
         else
           Resque.enqueue(CompleteAuction, item_id)
           sleep(2.seconds)
