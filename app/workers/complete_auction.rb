ca29bbab0d46037f59976389ef53702b12eb1515
James <shoutsid@gmail.com>
2013-05-25 04:53:28 +0100

    Push to the back of queue, sleep to help CPU

--- app/workers/complete_auction.rb
+++ app/workers/complete_auction.rb
@@ -1,4 +1,6 @@
 class CompleteAuction
+  ## TODO:- Add some payment method etc
+  
   @queue = :check_complete_auction_queue
 
   def self.perform(item_id)
@@ -8,12 +10,11 @@ class CompleteAuction
         if item.expired?
           puts "name: #{item.name}, item_id: #{item.id}, bid_id: #{item.highest_bid.id if item.highest_bid}, bid_amount: #{item.highest_bid.amount if item.highest_bid}"
           item.close_auction
-
-          ## TODO:- Add some payment method here
-          #         Create some live feed data of all auctions
+        else
+          Resque.enqueue(CompleteAuction, item_id)
+          sleep(2.seconds)
+          break
         end
-        break if item.closed?
-        sleep 20
       end
     end
   end
