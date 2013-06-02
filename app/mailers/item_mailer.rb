6155fc28e5b3d183e6fcfde13b6c1c62442b2b65
James <shoutsid@gmail.com>
2013-06-02 21:23:12 +0100

    some quick emails for sold/not_sold items

--- app/mailers/item_mailer.rb
+++ app/mailers/item_mailer.rb
@@ -1,13 +1,26 @@
 class ItemMailer < ActionMailer::Base
   default from: "shoutsid@gmail.com"
 
-  def item_sold(item, bid)
+  def item_sold_owner(item, bid)
     @bid = bid
     @bid_user = bid.user
     @item = item
     @item_user = item.user
-
     mail(to: @item_user.email, subject: "#{@item.name}: Sold! Congrats")
   end
-  
+
+  def item_sold_bidder(item, bid)
+    @bid = bid
+    @bid_user = bid.user
+    @item = item
+    @item_user = item.user
+    mail(to: @bid_user.email, subject: "#{@item.name}: Won! Congrats")
+  end
+
+  def item_not_sold_owner(item)
+    @item = item
+    @item_user = item.user
+    mail(to: @item_user.email, subject: "#{@item.name}: Failed Miserably :(")
+  end
+
 end
