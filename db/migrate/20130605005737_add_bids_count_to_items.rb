8f39204999cf23e6707f74a243ef55a41962dde8
James <shoutsid@gmail.com>
2013-06-05 03:46:34 +0100

    Item, not user

--- db/migrate/20130605005737_add_bids_count_to_items.rb
+++ db/migrate/20130605005737_add_bids_count_to_items.rb
@@ -3,7 +3,7 @@ class AddBidsCountToItems < ActiveRecord::Migration
     add_column :items, :bids_count, :integer, default: 0
 
     Item.find_each do |item|
-      User.reset_counters(item.id, :bids)
+      Item.reset_counters(item.id, :bids)
     end
   end
 end
