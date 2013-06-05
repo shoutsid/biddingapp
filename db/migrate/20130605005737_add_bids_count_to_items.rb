2bcafb08e69bf82a94722ce1ac7f9c4eb7e84dbc
James <shoutsid@gmail.com>
2013-06-05 03:43:16 +0100

    Correct cache count migration, fixed bug on deploy

--- db/migrate/20130605005737_add_bids_count_to_items.rb
+++ db/migrate/20130605005737_add_bids_count_to_items.rb
@@ -1,9 +1,9 @@
 class AddBidsCountToItems < ActiveRecord::Migration
   def change
     add_column :items, :bids_count, :integer, default: 0
+
     Item.find_each do |item|
-      item.update_attribute(:bids_count, item.bids.length)
-      item.save
+      User.reset_counters(item.id, :bids)
     end
   end
 end
